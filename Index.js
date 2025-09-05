6d4b39ab30827104fb658081fb1f7b9ab0a96b96
Anuriamaka <nworahanuriamaka@gmail.com>
2025-09-06 00:07:41 +0100

    Assignment 2 Solutions

---
+++ Index.js
@@ -0,0 +1,14 @@
+let num1 = 20;
+let num2 = 10;
+let name = "Anuriamaka";
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2
+let division = num1 / num2
+
+console.log("Addition (num1 + num2):", addition);
+console.log("subtraction (num1 + num2):", subtraction);
+console.log("multiplication (num1 * num2):", multiplication);
+console.log("division (num1 / num2):", division);
+console.log(`Hello ${name}, the result of ${num1} + ${num2} is ${addition}`);
